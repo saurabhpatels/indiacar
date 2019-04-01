0cbc07eaa7b7af89840f2f325ce6122fefa6ebdd
Saurabh <saurabh.patel.mi@gmail.com>
2019-04-01 19:05:24 +0530

    All Set 1st April

---
+++ src/app/main/log.js
@@ -0,0 +1,46 @@
+/*
+DAILY WORK REPORT
+
+Name: Saurabh Patel
+Date: 01-04-2019
+StartTime: 9:30 AM
+LunchTime: 1:30 PM TO 2:15 PM
+EndTime: 07:10 PM
+
+#LearnAngular: [8H:00M]
+[30M] - Solve User's State Issue.
+        -Fixed Automatic Redirect To Login Page
+[30M] - Created CanActivate method to Prevent Access of URL.
+[1H]  - Read FireBase Doc
+        -Authentication
+        -RealtimeDatabase
+        -Cloud Firestore
+        -Cloud Functions
+[1H ] - Created Two Guard TypeScripts
+        -AuthGuard
+        -InnerPages
+[30m] - Build Separate Route Module To Utilize Flow
+        -Remove Route From Each Module And Created Singleton.
+[30M] - Implemented Forgot Password Api.
+[30M] - Implemented Verify-Email Api.
+[30M] - Maintained Logged in State of Firebase User in localStorage.
+[30M] - Created FaceBook Client App Id & Sceret Id.
+        -Enable Firebase Settings For FaceBook
+        -Get AppId & Seceret Key For App
+[30M] - Implemented Facebook Login Api
+[30m] - Design The Ui Of Dashboard.
+[30m] - Implemented CrudApi After Login State
+[30m] - Solve Routing Issue.
+[30M] - Set NGX-Toaser Module For Warnings & Notification.
+
+
+#Tommorow's Task [02/04/2019]
+-Perform opreations With Realtime Database
+
+
+
+
+
+
+
+ */
